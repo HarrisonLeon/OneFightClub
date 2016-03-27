52e58d6e9a537d5046afe02b5a4acb55503738a2
Harrison Leon <hleon@usc.edu>
2016-03-27 13:26:12 -0700

    Changed inheritance for Enemy

--- core/src/game/videogrames/onefightclub/actors/Enemy.java
+++ core/src/game/videogrames/onefightclub/actors/Enemy.java
@@ -6,13 +6,14 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.utils.Array;
 
-public class Enemy extends OFCSprite {
+public class Enemy extends MovingSprite
+{
+	public static final String PLAYER_FILEPATH = "images/bunny.png";
 
-	public static final String	PLAYER_FILEPATH	= "images/bunny.png";
-	
-	public Enemy(Body body) {
+	public Enemy(Body body)
+	{
 		super(body);
-		
+
 		Texture t = new Texture(Gdx.files.internal(PLAYER_FILEPATH));
 		Array<TextureRegion> sprites = new Array<TextureRegion>(TextureRegion.split(t, 32, 32)[0]);
 
