8bbb06beb91cf1d4d44fb60647a74a4f43b17de9
Brenden Donlon <donlon@usc.edu>
2016-03-27 12:55:52 -0700

    Implemented enemy spawning, loading texture, and physics box

--- core/src/game/videogrames/onefightclub/actors/Enemy.java
+++ core/src/game/videogrames/onefightclub/actors/Enemy.java
@@ -1,10 +1,21 @@
 package game.videogrames.onefightclub.actors;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.utils.Array;
 
-public class Enemy extends MovingSprite {
+public class Enemy extends OFCSprite {
 
+	public static final String	PLAYER_FILEPATH	= "images/bunny.png";
+	
 	public Enemy(Body body) {
 		super(body);
+		
+		Texture t = new Texture(Gdx.files.internal(PLAYER_FILEPATH));
+		Array<TextureRegion> sprites = new Array<TextureRegion>(TextureRegion.split(t, 32, 32)[0]);
+
+		setAnimation(1 / 12.0f, sprites);
 	}
 }
