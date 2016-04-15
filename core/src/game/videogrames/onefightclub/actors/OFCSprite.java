cc7ed174d495425a80990a10c1d85170c905690d
Harrison Leon <hleon@usc.edu>
2016-04-15 00:54:26 -0700

    Player character upgrade
    Directional Facing | Walk, Attack, Idle Animations | Sword Physics/Sensors

--- core/src/game/videogrames/onefightclub/actors/OFCSprite.java
+++ core/src/game/videogrames/onefightclub/actors/OFCSprite.java
@@ -43,12 +43,15 @@ public abstract class OFCSprite {
 		return height;
 	}
 
+	public boolean getFacingRight() {
+		return facingRight;
+	}
+
 	public void setFacingRight(boolean b) {
 		this.facingRight = b;
 	}
 
 	public void setAnimation(float frameDuration, Array<TextureRegion> keyFrames) {
-		// stateTime = 0.0f;
 		animation = new Animation(frameDuration, keyFrames);
 		width = keyFrames.get(0).getRegionWidth();
 		height = keyFrames.get(0).getRegionHeight();
@@ -57,14 +60,18 @@ public abstract class OFCSprite {
 	public void render(SpriteBatch sb) {
 		stateTime += Gdx.graphics.getDeltaTime();
 		TextureRegion currFrame = animation.getKeyFrame(stateTime, true);
+
+		// draw the current frame
 		sb.begin();
+
+		// flip frames according to direction sprite is facing
 		if (!facingRight && !currFrame.isFlipX()) {
 			currFrame.flip(true, false);
 		} else if (facingRight && currFrame.isFlipX()) {
 			currFrame.flip(true, false);
 		}
-		sb.draw(currFrame, body.getPosition().x * Constants.PPM - width / 2,
-				body.getPosition().y * Constants.PPM - height / 2);
+
+		sb.draw(currFrame, getPosition().x * Constants.PPM - width / 2, getPosition().y * Constants.PPM - height / 2);
 		sb.end();
 	}
 
--- core/src/game/videogrames/onefightclub/actors/Weapon.java
+++ core/src/game/videogrames/onefightclub/actors/Weapon.java
@@ -1,21 +1,20 @@
 package game.videogrames.onefightclub.actors;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.utils.Array;
 
 public class Weapon extends StillSprite {
 
-    public static final String PLAYER_FILEPATH = "images/soldierwalk.png";
+	private boolean active;
 
-    public Weapon(Body body) {
+	public Weapon(Body body) {
 		super(body);
-	
-		Texture t = new Texture(Gdx.files.internal(PLAYER_FILEPATH));
-		Array<TextureRegion> sprites = new Array<TextureRegion>(TextureRegion.split(t, 80, 64)[0]);
-	
-		setAnimation(1 / 12.0f, sprites);
-    }
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
 }
